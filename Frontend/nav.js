00e022364c796c19de2d8f1ff59565155d0505bf
Jéssica Steffany <jessicasteffany598@gmail.com>
2025-12-09 13:26:37 -0300

    primeiro commit

---
+++ Frontend/nav.js
@@ -0,0 +1,21 @@
+function irGaleria(){
+window.location.href="galeria.html";
+}
+function inicio(){
+window.location.href="paginaInicial.html";
+}
+function irLession(){
+window.location.href="aprendizado.html";
+}
+function irPerfil(){
+window.location.href="perfil.html";
+}
+function irConfig(){
+    window.location.href="configuracoes.html";
+}
+function voltar(){
+    window.history.back();
+}
+function curso(){
+    window.location.href="aprendizado.html";
+}
